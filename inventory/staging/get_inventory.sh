ea26f45902860af340fe7fa989ba86e2781372d1
Amedee Van Gasse ✨🌈🦄 <amedee-github@amedee.be>
2024-06-18 13:17:17 +0200

    Configure WordPress
    Use WP-CLI to write a wp-config.php

--- inventory/staging/get_inventory.sh
+++ inventory/staging/get_inventory.sh
@@ -18,6 +18,7 @@ all:
       ansible_host: $IP
       ansible_user: vagrant
       ansible_ssh_private_key_file: .vagrant/machines/default/virtualbox/private_key
+      webserver_db_host: localhost
   children:
     mailservers:
       hosts:
