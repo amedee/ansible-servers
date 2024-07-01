2e9645764b35d0996a2b8c3d228642e4a22db28c
Amedee Van Gasse ✨🌈🦄 <amedee-github@amedee.be>
2024-07-01 02:49:15 +0200

    Move ansible_ssh_private_key_file to group_vars
    Makes get_inventory.sh shorter

--- inventory/staging/get_inventory.sh
+++ inventory/staging/get_inventory.sh
@@ -16,7 +16,6 @@ all:
   hosts:
     ${HOSTNAME}:
       ansible_host: $IP
-      ansible_ssh_private_key_file: .vagrant/machines/default/virtualbox/private_key
   children:
     mailservers:
       hosts:
