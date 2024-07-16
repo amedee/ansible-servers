5d79a2a6c37877425a70dbc9a590635d1231bbd0
Amedee Van Gasse ✨🌈🦄 <amedee-github@amedee.be>
2024-07-16 19:29:57 +0200

    Install roles and collections using requirements.xml
    That way I only need to edit one file if I want to add a dependency

--- deploy_to_staging.sh
+++ deploy_to_staging.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 vagrant up --provision
+ansible-galaxy install -r requirements.yml
 ansible-playbook playbooks/site.yml -i inventory/staging/hosts.yml --vault-password-file ~/.vault_pass.txt
