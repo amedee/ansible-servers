9640b264d451a0380d064dd3a4b115468bd7192a
Amedee Van Gasse ✨🌈🦄 <amedee-github@amedee.be>
2024-10-20 19:27:38 +0200

    Update Vagrantbox to Ubuntu 24.04 LTS (Noble Numbat)

--- deploy_to_staging.sh
+++ deploy_to_staging.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-vagrant up --provision
+vagrant up --provision --provider=virtualbox
 ansible-galaxy install -r requirements.yml
 ansible-playbook playbooks/site.yml -i inventory/staging/hosts.yml --vault-password-file ~/.vault_pass.txt -v
