196889cfbcf898a13a65b7cb31426a75009c2a00
Amedee Van Gasse <amedee-github@amedee.be>
2026-04-16 15:33:15 +0200

    Fix docstring formatting in ses_sendmail.py

--- roles/postfix/files/ses_sendmail.py
+++ roles/postfix/files/ses_sendmail.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""SES sendmail wrapper.
+"""
+SES sendmail wrapper.
 
 Reads a raw email from stdin and submits it to Amazon SES using the
 `send_raw_email` API call. Designed to be called by Postfix as a transport.
@@ -46,7 +47,8 @@ def extract_recipients(msg: Message) -> List[str]:
 
 
 def send_via_ses(msg: Message, recipients: List[str]) -> str:
-    """Send a raw email message via Amazon SES.
+    """
+    Send a raw email message via Amazon SES.
 
     :param msg: Email Message object.
     :param recipients: List of recipients.
